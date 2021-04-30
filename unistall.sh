8acdeb4f7c3ad604fe1aaf6338de341441d86c6a
navy1978 <ivan.morelli@gmail.com>
2021-04-30 17:39:19 +0200

    Update unistall.sh

--- unistall.sh
+++ unistall.sh
@@ -5,21 +5,30 @@
 . /etc/profile
 
 
-INSTALL_PATH="/storage/retrorun_flycast32"
-BINARY="retrorun_flycast32"
+INSTALL_PATH="/storage/retrorun"
+BINARY="retrorun"
 CFG="/storage/.emulationstation/es_systems.cfg"
+TMP_CFG_DEL="/storage/.emulationstation/es_systems_del.cfg"
 START_SCRIPT="$BINARY.sh"
 
 MYARCH="armhf"
 
-if grep -q '<name>dreamcast</name>' "$CFG"
+if grep -q 'retrorun' "$CFG"
 then
-	xmlstarlet ed -L -P -d "/systemList/system[name='dreamcast']" $CFG
+	xmlstarlet ed -d '/systemList/system[name="dreamcast"]/emulators/emulator[@name="retrorun"]' $CFG > $TMP_CFG_DEL
+	rm $CFG
+	mv $TMP_CFG_DEL $CFG
+	xmlstarlet ed -d '/systemList/system[name="naomi"]/emulators/emulator[@name="retrorun"]' $CFG > $TMP_CFG_DEL
+	rm $CFG
+	mv $TMP_CFG_DEL $CFG
+	xmlstarlet ed -d '/systemList/system[name="atomiswave"]/emulators/emulator[@name="retrorun"]' $CFG > $TMP_CFG_DEL
+	rm $CFG
+	mv $TMP_CFG_DEL $CFG
 fi
 
-if [ -f /storage/.config/emulationstation/scripts/retrorun_flycast32.sh ]
+if [ -f /storage/.config/emulationstation/scripts/retrorun.sh ]
 then
-  rm -f /storage/.config/emulationstation/scripts/retrorun_flycast32.sh
+  rm -f /storage/.config/emulationstation/scripts/retrorun.sh
 fi
 
 rm -rf ${INSTALL_PATH}
